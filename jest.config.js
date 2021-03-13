18e45759a5b79cd875079ad476f40e34642bc6a1
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-03-13 20:31:03 +0100

    updated

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: "@vue/cli-plugin-unit-jest",
+  moduleFileExtensions: ["js", "json", "vue"],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    ".*\\.(vue)$": "vue-jest",
+    "^.+\\.vue$": "vue-jest",
+  },
+};
